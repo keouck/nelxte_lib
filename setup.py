1145f46913039d9e138b4da7488a8fc0f8b23d6c
Amay Agarwal <amayagarwal428@gmail.com>
2021-01-11 21:26:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='nelxte',
     packages=find_packages(include=['nelxte']),
     version='1.1',
-    download_url = 'https://github.com/amay428/nelxte_lib/archive/1.0.tar.gz',
+    download_url = 'https://github.com/amay428/nelxte_lib/archive/1.1.tar.gz',
     description='Nelxte is a fast and open source python library for checking password strength. 
     author='Amay Agarwal',
     license='MIT',

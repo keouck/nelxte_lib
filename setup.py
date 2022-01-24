22ff37ecd10a5460d9e768a30eb91a5d86cea05f
balzaen <imerminu@tutanota.com>
2022-01-24 12:31:00 +0530

    Update username to Keouck

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ setup(
     name='nelxte',
     packages=find_packages(include=['nelxte']),
     version='1.1',
-    download_url = 'https://github.com/amay428/nelxte_lib/archive/1.1.tar.gz',
-    description='Nelxte is a fast and open source python library for checking password strength. 
-    author='Amay Agarwal',
+    download_url = 'https://github.com/keouck/nelxte_lib/archive/1.1.tar.gz',
+    description='Nelxte is a fast and open source python library for checking password strength.'
+    author='keouck',
     license='MIT',
 )
